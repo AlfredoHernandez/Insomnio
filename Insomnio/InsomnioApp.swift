3889d31a1d4cbf139c7289ee90427369f0f769d2
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2026-02-15 18:02:09 -0600

    Initial Commit

---
+++ Insomnio/InsomnioApp.swift
@@ -0,0 +1,14 @@
+//
+// Copyright © 2026 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct InsomnioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
